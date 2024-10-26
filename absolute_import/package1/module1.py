fe3dc149fc011d7ec3247797b330b371fe5d437e
Kentaro Machida <kenkenm120319@gmail.com>
2024-10-26 14:28:10 +0900

    絶対パスのテストを追加

---
+++ absolute_import/package1/module1.py
@@ -0,0 +1,24 @@
+import package2.module2
+import package2.package2_1.module2_1
+import module0  # 階層が上のモジュールでも絶対パスでimportできる
+
+# エラーが発生するimport
+# import module2  絶対パスの起点がabsolute_importであるため、module2はimportできない
+
+def func_1(called_from: str):
+    print(f'{__name__}.func_1 is called from {called_from}')
+    module0.func_0(called_from=__name__)
+    package2.module2.func_2(called_from=__name__)
+    package2.package2_1.module2_1.func_2_2(called_from=__name__)
+
+if __name__ == '__main__':
+    """
+    モジュール単体で実行された場合の処理（単体テストをイメージ）
+    このモジュールだけを直接実行したい場合は、プロジェクトのルートディレクトリ(absolute_import配下)で以下のコマンドを実行する
+    python -m package1.module1
+    もし、以下のコマンドで実行すると、エラーmodule1が起点のため、module2がimportできない
+    python package1.module1.py
+    つまり-mオプションをつけて「モジュールとして実行する」というのは、モジュールを呼び出すであろう
+    スクリプトの位置を絶対パスの起点にするということ
+    """
+    func_1(called_from=__name__)
