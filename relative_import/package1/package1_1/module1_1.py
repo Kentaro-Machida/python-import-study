9d89a533046aadd92ed45fa8b60496a5e5e5a8f1
Kentaro Machida <kenkenm120319@gmail.com>
2024-10-27 11:48:49 +0900

    親ディレクトリへの相対importを追加

--- relative_import/package1/package1_1/module1_1.py
+++ relative_import/package1/package1_1/module1_1.py
@@ -1,2 +1,5 @@
+from ..package1_2 import module1_2
+
 def func_1_1(called_from: str):
     print(f'{__name__}.func_1_1 is called from {called_from}')
+    module1_2.func_1_2(__name__)
---
+++ relative_import/package1/package1_2/module1_2.py
@@ -0,0 +1,2 @@
+def func_1_2(called_from: str):
+    print(f'{__name__}.func_1_2 is called from {called_from}')
